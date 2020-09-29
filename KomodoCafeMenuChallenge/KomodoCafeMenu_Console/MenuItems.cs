0ed8a732ce248a37434752441d733ae8dbe08387
BHollis1 <betsy.hollis1@gmail.com>
2020-09-29 11:29:50 -0400

    First Push

---
+++ KomodoCafeMenuChallenge/KomodoCafeMenu_Console/MenuItems.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KomodoCafeMenu_Console
+{
+    public class MenuItems
+    {
+        public int MealNumber { get; set; }
+        public string MealName { get; set; }
+        public string MealDescription { get; set; }
+        public string ListOfIngredients { get; set; }
+        public float MealPrice { get; set; }
+        public MenuItems() { }
+        public MenuItems(int mealNum, string mealName, string mealDesc, string listOfIng, float mealPrice)
+        {
+            MealNumber = mealNum;
+            MealName = mealName;
+            MealDescription = mealDesc;
+            ListOfIngredients = listOfIng;
+            MealPrice = mealPrice;
+        }
+
+      
+       
+    }
+}
---
+++ KomodoCafeMenuChallenge/KomodoCafeMenu_Console/MenuRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KomodoCafeMenu_Console
+{
+    public class MenuRepository
+    {
+        protected readonly List<MenuRepository> _menuDirectory = new List<MenuRepository>();
+
+        public bool CreateNewMenuItems(MenuRepository newItems)
+        {
+            int startingCount = _menuDirectory.Count;
+            _menuDirectory.Add(newItems);
+            bool wasCreated = (_menuDirectory.Count > startingCount) ? true : false;
+            return wasCreated;
+        }
+
+        public List<MenuRepository> GetAllMenuItems()
+        {
+            return _menuDirectory;
+        }
+    }
+}
